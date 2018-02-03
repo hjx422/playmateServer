e29e27b6e980a067fa3f8d78b727d1015c150cc1
hjx422 <hjx422@gmail.com>
2018-02-03 16:29:35 +0800

    add： server.js 添加路由

--- js/server.js
+++ js/server.js
@@ -2,12 +2,18 @@
  * Created by Administrator on 2017/9/3.
  */
 const Koa = require('koa')
-const app = new Koa()
-const PORT = 3001
+const Router = require('koa-router')
 
-app.use(ctx => {
-    ctx.response.body = 'Hello, World!'
+const app = new Koa()
+const router = new Router()
+
+const PORT = 3000
+
+router.get('/', (ctx, next) => {
+    ctx.body = 'Hello World!'
 })
+
+app.use(router.routes()).use(router.allowedMethods())
 
 app.listen(PORT)
 console.log(`Koa started on port ${PORT}`)
