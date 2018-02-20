c76bfa34cdfcb3f3f815032a8a9fc63f49399c10
hjx422 <hjx422@gmail.com>
2018-02-20 23:24:48 +0800

    update: 程序入口由 server.js 变成 src/index.js； require -> import ;

--- js/src/index.js
+++ js/src/index.js
@@ -1,3 +1,18 @@
 /**
  * Created by hjx on 2/14/2018.
  */
+import Koa from 'koa'
+import Router from 'koa-router'
+import { helloworld } from './controllers/helloworld.js'
+
+const app = new Koa()
+const router = new Router()
+
+const PORT = 3000
+
+router.get('/', helloworld)
+
+app.use(router.routes()).use(router.allowedMethods())
+
+app.listen(PORT)
+console.log(`Koa started on port ${PORT}`)
