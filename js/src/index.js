b2cdc9db53ab7bf1851a4ab72dd4a67084276d03
hjx422 <hjx422@gmail.com>
2018-06-25 08:31:49 +0800

    update: koa-cors -> koa2-cors

--- js/src/index.js
+++ js/src/index.js
@@ -3,7 +3,7 @@
  */
 import Koa from 'koa'
 import bodyParser from 'koa-bodyparser'
-import cors from 'koa-cors'
+import cors from 'koa2-cors'
 
 import router from './routers'
 import './db/db.js'
@@ -15,7 +15,16 @@ const PORT = 3000
 // 配置数据解析中间件
 app.use(bodyParser({ enableTypes: ['json', 'form', 'text'] }))
 
-app.use(cors())
+app.use(cors({
+  origin: (ctx) => {
+    return 'http://localhost:8080'
+  },
+  exposeHeaders: ['WWW-Authenticate', 'Server-Authorization'],
+  maxAge: 5,
+  credentials: true,
+  //allowMethods: ['GET', 'POST', 'DELETE'],
+  allowHeaders: ['Content-Type', 'Authorization', 'Accept']
+}))
 
 // 配置路由中间件
 app.use(router.routes()).use(router.allowedMethods())
